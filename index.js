c27f9327c036afd8adc33880a688d1ffdec2b78d
lawrence254 <lawrenslava1@gmail.com>
2022-02-16 09:57:35 +0300

    Complete implementation of the tests for the palindrome

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("") === word ? true : false;
 }
 
 /* 
   Add your pseudocode here
+  1. Split the input string
+  2. Use the built in reverse method to reverse the resulting string after split
+  3. Use the built in join function to to create a string from the array.
+  4. check whether the resulting string is exactly the same as the input word
+  5. return a boolean that indicates whether the string is a  palindrom(true) or not(false)
 */
 
 /*
